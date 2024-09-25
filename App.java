5acc69fa384c1fcf734a0f5c058af47d814cf0cc
mochawoof <julianherbert.2007@gmail.com>
2024-09-25 16:00:47 -0400

    update icon

--- App.java
+++ App.java
@@ -8,6 +8,10 @@ import org.fife.ui.rsyntaxtextarea.*;
 
 import java.util.function.IntConsumer;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 class App extends JFrame {
     private void setTheme(String theme) {
         try {
@@ -19,9 +23,23 @@ class App extends JFrame {
         }
     }
     
+    private String getMimeType(String filePath) {
+        String type = null;
+        try {
+            type = Files.probeContentType(Paths.get(filePath));
+        } catch (Exception e) {
+            System.err.println("Mime type probe failed for " + filePath);
+            e.printStackTrace();
+        }
+        if (type == null) {
+            type = "text/plain";
+        }
+        return type;
+    }
+    
     private void addFileTab(String filePath) {
         RSyntaxTextArea textArea = new RSyntaxTextArea(20, 60);
-        textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
+        textArea.setSyntaxEditingStyle(getMimeType(filePath));
         textArea.setCodeFoldingEnabled(true);
         
         RTextScrollPane pane = new RTextScrollPane(textArea);
@@ -46,6 +64,9 @@ class App extends JFrame {
         // Menu bar
         JMenuBar menuBar = new JMenuBar();
         setJMenuBar(menuBar);
+        
+        JMenu projectMenu = new JMenu("Project");
+        menuBar.add(projectMenu);
         
         JMenu fileMenu = new JMenu("File");
         menuBar.add(fileMenu);
